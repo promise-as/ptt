c77a4e05a0fead20aa8f8d28e847775f080f1b81
Hong Baojin <13809735212@163.com>
2019-06-07 01:37:38 +0800

    write to client-help

--- dist/js/index.js
+++ dist/js/index.js
@@ -3,16 +3,17 @@
 $(function () {
   function tabActive(ele) {
     $(ele).each(function () {
-      var that = this;
-      $(that).mouseenter(function () {
-        $(that).addClass('active').siblings().removeClass('active');
+      $(this).mouseenter(function () {
+        $(this).addClass('active').siblings().removeClass('active');
       });
     });
   }
-  /*头部右侧导航*/
+  /*首页 nav*/
   tabActive('.nav a');
+
+  /*首页 app*/
+  tabActive('.app .app-item');
+
   /*客户帮助 和 文章详情的 tab*/
   tabActive('.main_tab_body .tab_body_item');
-  /*平台 app*/
-  tabActive('.app .app-item');
 });
